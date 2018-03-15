90496e9c00a9c78041d3a23cc35793ce682c9215
mattmlz <matthieu.toussaint@hetic.net>
2018-03-15 14:09:16 +0100

    :sparkles: add geolocation with Google Maps geolocation API

--- index.php
+++ index.php
@@ -1,26 +1,5 @@
 <?php 
-$curl = curl_init();
-
-curl_setopt($curl, CURLOPT_URL,"https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyCBhR09cwrKOQb98S7lSTm2yMG8lrIAoTw");
-curl_setopt($curl, CURLOPT_HTTPHEADER, array(  
-  'Content-Type: application/json',  
-  'Content-Length: 0',)  
-); 
-
-//Use POST Method as asked by Google API
-curl_setopt($curl, CURLOPT_POST, 1);
-
-// receive server response ...
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-
-$server_output = curl_exec($curl);
-
-curl_close ($curl);
-
-//To test the answer
-echo '<pre>';
-var_dump($server_output);
-echo '</pre>';
+  include 'includes/google-geolocation-api.php';
 ?>
 
 <!DOCTYPE html>
@@ -33,15 +12,10 @@ echo '</pre>';
 </head>
 <body>
 
-  <!-- Form -->
-  <form action="#" method="post">
-    <input type="submit" name="position">
-  </form>
-
   <div class="position">
     <h3>Your position</h3>
-    <h4>Latitude: <?= $lat; ?></h4>
-    <h4>Longitude: <?= $lon; ?></h4>
+    <h4>Latitude: <?= 'gros' ?></h4>
+    <h4>Longitude: <?= 'pd' ?></h4>
   </div>
   
 </body>
