328ccfdfaa8e71802577a257e3ace283a7d7b019
mattmlz <matthieu.toussaint@hetic.net>
2018-03-16 14:12:39 +0100

    separate weather API call in an other file

--- index.php
+++ index.php
@@ -1,13 +1,7 @@
 <?php 
   include 'includes/google-geolocation-api.php';
+  include 'includes/weather-api.php';
 
-  $url = 'http://api.openweathermap.org/data/2.5/forecast?lat='.$lat.'&lon='.$lng.'&units=metric&appid=f534d5a49e05d138b0ced4f7deaa9435';
-
-  $forecast = json_decode(file_get_contents($url), true);
-
-  echo '<pre>';
-  print_r($forecast);
-  echo '</pre>';
 ?>
 
 <!DOCTYPE html>
