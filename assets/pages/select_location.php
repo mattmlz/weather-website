780143145d59a8fe612a26e511012ffe274cb5d8
mattmlz <matthieu.toussaint@hetic.net>
2018-03-20 23:21:52 +0100

    starting add styles and other pages

--- assets/pages/select_location.php
+++ assets/pages/select_location.php
@@ -1,6 +1,6 @@
 <?php 
-  include 'assets/includes/google-geolocation-api.php';
-  include 'assets/includes/weather-api.php';
+  include '../includes/google-geolocation-api.php';
+  include '../includes/weather-api.php';
 ?>
 
 <!DOCTYPE html>
@@ -9,17 +9,17 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="assets/css/reset.css">
+  <link rel="stylesheet" href="../css/reset.css">
   <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-  <link rel="stylesheet" href="assets/css/main.css">
-  <link rel="apple-touch-icon" sizes="180x180" href="/assets/img/favicon/apple-touch-icon.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="/assets/img/favicon/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="/assets/img/favicon/favicon-16x16.png">
-  <link rel="manifest" href="/assets/img/favicon/site.webmanifest">
-  <link rel="mask-icon" href="/assets/img/favicon/safari-pinned-tab.svg" color="#5bbad5">
-  <link rel="shortcut icon" href="/assets/img/favicon/favicon.ico">
+  <link rel="stylesheet" href="../css/main.css">
+  <link rel="apple-touch-icon" sizes="180x180" href="../img/favicon/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="../img/favicon/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="../img/favicon/favicon-16x16.png">
+  <link rel="manifest" href="../img/favicon/site.webmanifest">
+  <link rel="mask-icon" href="../img/favicon/safari-pinned-tab.svg" color="#5bbad5">
+  <link rel="shortcut icon" href="../img/favicon/favicon.ico">
   <meta name="msapplication-TileColor" content="#00aba9">
-  <meta name="msapplication-config" content="/assets/img/favicon/browserconfig.xml">
+  <meta name="msapplication-config" content="../img/favicon/browserconfig.xml">
   <meta name="theme-color" content="#ffffff">
   <title>Weather companion</title>
 </head>
@@ -29,10 +29,10 @@
   <div class="header_menu">
     <p class="name">Weather Companion</p>
     <div class="right">
-      <div class="home"><a href="./index.php">Home</a></div>
-      <div class="bad_location"><a href="./assets/pages/select_location.php">Not the good location ?</a></div>
-      <div class="contact"><a href="./assets/pages/contact.php">Contact</a></div>
-      <img src="assets/img/logo_little.png" alt="logo" class="header_logo">
+      <div class="home"><a href="../../index.php">Home</a></div>
+      <div class="bad_location"><a href="../pages/select_location.php">Not the good location ?</a></div>
+      <div class="contact"><a href="../pages/contact.php">Contact</a></div>
+      <img src="../img/logo_little.png" alt="logo" class="header_logo">
     </div>
   </div>
 </header>
