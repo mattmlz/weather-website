194a4bd2a670d10dbf53b0957e47ab9d64b02f56
mattmlz <matthieu.toussaint@hetic.net>
2018-03-21 09:17:53 +0100

    comments

--- index.php
+++ index.php
@@ -9,9 +9,11 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <!-- CSS -->
   <link rel="stylesheet" href="assets/css/reset.css">
   <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
   <link rel="stylesheet" href="assets/css/main.css">
+  <!-- FAVICON -->
   <link rel="apple-touch-icon" sizes="180x180" href="/assets/img/favicon/apple-touch-icon.png">
   <link rel="icon" type="image/png" sizes="32x32" href="/assets/img/favicon/favicon-32x32.png">
   <link rel="icon" type="image/png" sizes="16x16" href="/assets/img/favicon/favicon-16x16.png">
@@ -24,7 +26,7 @@
   <title>Weather companion</title>
 </head>
 <body>
-
+<!-- BASE HEADER -->
 <header>
   <div class="header_menu">
     <p class="name">Weather Companion</p>
