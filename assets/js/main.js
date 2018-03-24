aa83e208f36d71ed8ae36da53656acf15f5db62c
mattmlz <matthieu.toussaint@hetic.net>
2018-03-24 11:56:51 +0100

    change style propeties

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,43 +24,43 @@ const current_day = daylist[day]
 if(current_day != 'Monday' ){
   for(index_monday=0; index_monday < $monday.length; index_monday++){
     const $mondays = $monday[index_monday]
-    $mondays.style.visibility = 'hidden'
+    $mondays.style.display = 'none'
   }
 }
 if(current_day != 'Tuesday'){
   for(index_tuesday=0; index_tuesday < $tuesday.length; index_tuesday++){
     const $tuesdays = $tuesday[index_tuesday]
-    $tuesdays.style.visibility = 'hidden'
+    $tuesdays.style.display = 'none'
   }
 }
 if(current_day != 'Wednesday'){
   for(index_wednesday=0; index_wednesday < $wednesday.length; index_wednesday++){
     const $wednesdays = $wednesday[index_wednesday]
-    $wednesdays.style.visibility = 'hidden'
+    $wednesdays.style.display = 'none'
   }
 }
 if(current_day != 'Thursday'){
   for(index_thursday=0; index_thursday < $thursday.length; index_thursday++){
     const $thursdays = $thursday[index_thursday]
-    $thursdays.style.visibility = 'hidden'
+    $thursdays.style.display = 'none'
   }
 }
 if(current_day != 'Friday'){
   for(index_friday=0; index_friday < $friday.length; index_friday++){
     const $fridays = $friday[index_friday]
-    $fridays.style.visibility = 'hidden'
+    $fridays.style.display = 'none'
   }
 }
 if(current_day != 'Saturday'){
   for(index_saturday=0; index_saturday < $saturday.length; index_saturday++){
     const $saturdays = $saturday[index_saturday]
-    $saturdays.style.visibility = 'hidden'
+    $saturdays.style.display = 'none'
   }
 }
 if(current_day != 'Sunday'){
   for(index_sunday=0; index_sunday < $sunday.length; index_sunday++){
     const $sundays = $sunday[index_sunday]
-    $sundays.style.visibility = 'hidden'
+    $sundays.style.display = 'none'
   }
 }
 
