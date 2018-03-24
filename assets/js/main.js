f3b7be6950d144630aaa769017d944ba322a10f2
mattmlz <matthieu.toussaint@hetic.net>
2018-03-24 11:14:54 +0100

    add js file

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+//Set variables
+const $monday = document.querySelectorAll('.Monday')
+let index_monday = 0
+const $tuesday = document.querySelectorAll('.Tuesday')
+let index_tuesday = 0
+const $wednesday = document.querySelectorAll('.Wednesday')
+let index_wednesday = 0
+const $thursday = document.querySelectorAll('.Thursday')
+let index_thursday = 0
+const $friday = document.querySelectorAll('.Friday')
+let index_friday = 0
+const $saturday = document.querySelectorAll('.Saturday')
+let index_saturday = 0
+const $sunday = document.querySelectorAll('.Sunday')
+let index_sunday = 0
+
+
+/* for(index_saturday=0; index < $saturday.length; index++){
+  const $saturdays = $saturday[index_saturday]
+  $saturdays.style.visibility = 'hidden'
+} */
