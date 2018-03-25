57784180b9aa0d8a8e6db3f044af713f3074629b
mattmlz <matthieu.toussaint@hetic.net>
2018-03-25 13:09:13 +0200

    refactoring js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,17 @@
 //Set variables
 const $monday = document.querySelectorAll('.Monday')
-let index_monday = 0
 const $tuesday = document.querySelectorAll('.Tuesday')
-let index_tuesday = 0
 const $wednesday = document.querySelectorAll('.Wednesday')
-let index_wednesday = 0
 const $thursday = document.querySelectorAll('.Thursday')
-let index_thursday = 0
 const $friday = document.querySelectorAll('.Friday')
-let index_friday = 0
 const $saturday = document.querySelectorAll('.Saturday')
-let index_saturday = 0
 const $sunday = document.querySelectorAll('.Sunday')
+let index_monday = 0
+let index_tuesday = 0
+let index_wednesday = 0
+let index_thursday = 0
+let index_friday = 0
+let index_saturday = 0
 let index_sunday = 0
 
 //Get current day
