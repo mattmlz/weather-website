9540f08ea98eab431f2bf943eaade87a6ce20fee
mattmlz <matthieu.toussaint@hetic.net>
2018-03-25 20:18:01 +0200

    create contact page

---
+++ assets/pages/contact.php
@@ -0,0 +1,42 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <!-- CSS -->
+  <link rel="stylesheet" href="../css/reset.css">
+  <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900" rel="stylesheet">
+  <link rel="stylesheet" href="../css/main.css">
+  <!-- FAVICON -->
+  <link rel="apple-touch-icon" sizes="180x180" href="../img/favicon/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="../img/favicon/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="../img/favicon/favicon-16x16.png">
+  <link rel="manifest" href="../img/favicon/site.webmanifest">
+  <link rel="mask-icon" href="../img/favicon/safari-pinned-tab.svg" color="#5bbad5">
+  <link rel="shortcut icon" href="../img/favicon/favicon.ico">
+  <meta name="msapplication-TileColor" content="#00aba9">
+  <meta name="msapplication-config" content="../img/favicon/browserconfig.xml">
+  <meta name="theme-color" content="#ffffff">
+  <title>Weather companion</title>
+</head>
+
+<body>
+<!-- BASE HEADER -->
+<header>
+  <div class="header_menu">
+    <p class="name">Weather Companion</p>
+    <div class="right">
+      <div class="home"><a href="../../index.php">Home</a></div>
+      <div class="bad_location"><a href="../pages/contact.php">Contact</a></div>
+      <img src="../img/logo_little.png" alt="logo" class="header_logo">
+    </div>
+  </div>
+</header>
+  
+</body>
+</html>
